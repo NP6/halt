ab466b27c3c1a5d0dc25756a6491a1f307be10f3
Larwin <larwin@gmail.com>
2018-02-16 00:25:16 +0100

    Update to version 2.2

--- src/monitor.cc
+++ src/monitor.cc
@@ -2,7 +2,11 @@
 
 Isolate* Monitor::isolate;
 Persistent<Context>* Monitor::context;
-Persistent<Function>* Monitor::callback;
+
+Persistent<Function>* Monitor::monitorInitialize;
+Persistent<Function>* Monitor::monitorResolve;
+Persistent<Function>* Monitor::monitorReject;
+Persistent<Function>* Monitor::monitorInstrumentation;
 
 
 void Monitor::Initialize() {
@@ -22,30 +26,87 @@ void Monitor::Hook(const FunctionCallbackInfo<Value>& args){
         return; 
     }
 
-    if (!args[0]->IsFunction()) { 
+    if (!args[0]->IsObject()) { 
         isolate->ThrowException(
             Exception::TypeError(
-                String::NewFromUtf8(isolate, "A function is expected")
             )
         ); 
         return; 
     }
 
-    Local<Function> value = Local<Function>::Cast(args[0]);
-    callback = new Persistent<Function>(isolate, value);
-    isolate->SetPromiseMonitor(Monitor::Executor);
+    // configuration
+    Local<Object> configuration = args[0]->ToObject();
+
+    // initialize
+    Local<Value> initialize = configuration->Get(String::NewFromUtf8(isolate, "initialize"));
+    monitorInitialize = initialize->IsFunction() ? new Persistent<Function>(isolate, Local<Function>::Cast(initialize)) : nullptr;
+    isolate->SetPromiseMonitorInitialize(monitorInitialize == nullptr ? nullptr : Monitor::ExecutePromiseInitialization);
+
+    // resolve
+    Local<Value> resolve = configuration->Get(String::NewFromUtf8(isolate, "resolve"));
+    monitorResolve = resolve->IsFunction() ? new Persistent<Function>(isolate, Local<Function>::Cast(resolve)) : nullptr;
+    isolate->SetPromiseMonitorResolve(monitorResolve == nullptr ? nullptr : Monitor::ExecutePromiseResolution);
+   
+    // reject
+    Local<Value> reject = configuration->Get(String::NewFromUtf8(isolate, "reject"));
+    monitorReject = reject->IsFunction() ? new Persistent<Function>(isolate, Local<Function>::Cast(reject)) : nullptr;
+    isolate->SetPromiseMonitorReject(monitorReject == nullptr ? nullptr : Monitor::ExecutePromiseRejection);
+
+    // instrument
+    Local<Value> instrument = configuration->Get(String::NewFromUtf8(isolate, "instrument"));
+    monitorInstrumentation = instrument->IsFunction() ? new Persistent<Function>(isolate, Local<Function>::Cast(instrument)) : nullptr;
+    isolate->SetPromiseMonitorPerform(monitorInstrumentation == nullptr ? nullptr : Monitor::ExecutePromiseInstrumentation);
 
 }
 void Monitor::Unhook(const FunctionCallbackInfo<Value>& args){
 
-    isolate->SetPromiseMonitor(nullptr);
+    isolate->SetPromiseMonitorInitialize(nullptr);
+    isolate->SetPromiseMonitorResolve(nullptr);
+    isolate->SetPromiseMonitorReject(nullptr);
+    isolate->SetPromiseMonitorPerform(nullptr);
 
 }
 
-Local<Value> Monitor::Executor(Local<Promise> promise, Local<Value> resolve, Local<Value> reject) {  
+void Monitor::ExecutePromiseInitialization(Local<Promise> promise, Local<Value> parent) {
 
     Local<Context> safe = context->Get(isolate);
-    if (promise->CreationContext() == safe){ return Undefined(isolate); }
+    if (promise->CreationContext() == safe){ return; }
+
+    Local<Value> argv[2] = { promise, parent };
+    monitorInitialize->Get(isolate)->Call(Undefined(isolate), 2, argv);
+  
+}
+
+void Monitor::ExecutePromiseResolution(Local<Promise> promise, Local<Value> result) {
+
+    Local<Context> safe = context->Get(isolate);
+    if (promise->CreationContext() == safe){ return; }
+    if (!result->IsPromise()){ return; }
+    if (Local<Promise>::Cast(result)->CreationContext() == safe){ return; }
+
+    Local<Value> argv[2] = { promise, result };
+    monitorResolve->Get(isolate)->Call(Undefined(isolate), 2, argv);
+  
+}
+
+void Monitor::ExecutePromiseRejection(Local<Promise> promise, Local<Value> reason) {
+
+    Local<Context> safe = context->Get(isolate);
+    if (promise->CreationContext() == safe){ return; }
+
+    Local<Value> argv[2] = { promise, reason };
+    monitorReject->Get(isolate)->Call(Undefined(isolate), 2, argv);
+  
+}
+
+Local<Value> Monitor::ExecutePromiseInstrumentation(
+    Local<Promise> promise, 
+    Local<Value> resolve, 
+    Local<Value> reject,
+    Local<Promise> deferred) {  
+
+    Local<Context> safe = context->Get(isolate);
+    if (promise->CreationContext() == safe && deferred->CreationContext() == safe){ return Undefined(isolate); }
 
     bool hasCallableResolve = resolve->IsFunction();
     if (hasCallableResolve && Local<Function>::Cast(resolve)->CreationContext() == safe){ return Undefined(isolate); }
@@ -54,11 +115,12 @@ Local<Value> Monitor::Executor(Local<Promise> promise, Local<Value> resolve, Loc
     if (hasCallableReject && Local<Function>::Cast(reject)->CreationContext() == safe){ return Undefined(isolate); }
 
     if (hasCallableResolve || hasCallableReject){
-        Local<Value> argv[3] = { promise, resolve, reject };
-        return callback->Get(isolate)->Call(Undefined(isolate), 3, argv);
+        Local<Value> argv[4] = { promise, resolve, reject, deferred };
+        return monitorInstrumentation->Get(isolate)->Call(Undefined(isolate), 4, argv);
     }
     else {
         return Undefined(isolate);
     }
 
 }
+
--- src/monitor.h
+++ src/monitor.h
@@ -17,9 +17,35 @@ class Monitor
 
     private:
         static Local<Value> Executor(Local<Promise> promise, Local<Value> resolve, Local<Value> reject);
+    
+        static void ExecutePromiseInitialization(
+            Local<Promise> promise, 
+            Local<Value> parent
+        );
+        
+        static void ExecutePromiseResolution(
+            Local<Promise> promise, 
+            Local<Value> result
+        );
+
+        static void ExecutePromiseRejection(
+            Local<Promise> promise, 
+            Local<Value> reason
+        );
+
+        static Local<Value> ExecutePromiseInstrumentation(
+            Local<Promise> promise, 
+            Local<Value> resolve, 
+            Local<Value> reject,
+            Local<Promise> deferred
+        );
+
         static Isolate* isolate;
         static Persistent<Context>* context;
-        static Persistent<Function>* callback;
+        static Persistent<Function>* monitorInitialize;
+        static Persistent<Function>* monitorResolve;
+        static Persistent<Function>* monitorReject;
+        static Persistent<Function>* monitorInstrumentation;
 
 };
     
